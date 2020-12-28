a25572f3e6e2273b98d3fdf294e8e12c1ba5d047
Jorgen-Jr <jorgenjr@outlook.com>
2020-12-28 16:59:34 -0300

    a2

--- functions/urbandictionary.js
+++ functions/urbandictionary.js
@@ -60,7 +60,7 @@ exports.handler = async event => {
         })
         .catch((error) => {
             console.log(error);
-            return { error, word, url };
+            return { word, url, ...error };
         });
 
     return {
