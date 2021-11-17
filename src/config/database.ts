9e5086ecfb8715c58a2633be25d18990280cc685
Jorgen-Jr <jorgenjr@outlook.com>
2021-11-17 12:49:22 -0300

    fix: Replaced "LIKE" query with "StartsWith".

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,20 +6,6 @@ mongoose.connect(process.env.DB_URI || "localhost", {
   dbName: process.env.DB_DATABASE,
 });
 
-// mongoose.set("debug", function (coll, method, query, doc, options) {
-//   let set = {
-//     coll: coll,
-//     method: method,
-//     query: query,
-//     doc: doc,
-//     options: options,
-//   };
-
-//   console.info({
-//     dbQuery: set,
-//   });
-// });
-
 mongoose.pluralize(null);
 
 export default mongoose;
--- src/controllers/DicioController.ts
+++ src/controllers/DicioController.ts
@@ -3,7 +3,7 @@ import Dicio from "../Models/dicio";
 export default {
   async get(word: string) {
     const result = await Dicio.find({
-      word: new RegExp(`.*${word}*.`),
+      word: new RegExp(`^.*${word}*.`),
     }).catch((err: any) => {
       console.log(err);
     });
